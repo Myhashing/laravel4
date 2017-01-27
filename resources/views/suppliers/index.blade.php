4f3db55d60be22aa769527b6be5003196f05f72c
arian <arianpourinfo@gmail.com>
2017-01-27 19:45:16 +0800

    New look  and fix looks problem

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -1,57 +1,83 @@
-<html>
-<head>
-    <title>Suppliers</title>
-</head>
-<body>
-<div class="panel panel-default">
-    <form action="/suppliers" method="post" class="form-horizontal">
-        {{ csrf_field() }}
-        <div class="form-group">
-            <label for="supplier-name" class="col-sm-3 control-label"> Supplier name</label>
-            <div class="col-sm-6">
-                <input type="text" name="name" id="supplier-name" class="form-control">
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="supplier-web" class="col-sm-3 control-label"> Supplier Web</label>
-            <div class="col-sm-6">
-                <input type="text" name="web" id="supplier-web" class="form-control">
-            </div>
-        </div>
-        <div>
-            <div class="col-sm-offset-3 col-sm-6">
-                <button type="submit" class="btn btn-default">
-                    <i class="fa fa-plus"></i>Add supplier
-                </button>
-            </div>
-        </div>
-    </form>
-</div>
+@extends('layouts.app')
 
-<div>
-    @if(count($suppliers1)>0)
-        @foreach($suppliers1 as $supplier)
-            <tr>
-                <!-- Supplier Name -->
-                <td class="table-text">
-                    <div>Supplier name : {{ $supplier->name }}</div>
-                </td>
-                <td class="table-text">
-                   Supplier Web:  {{ $supplier->web }}
-                </td>
-                <td>
-                    <form action="/suppliers/{{ $supplier->id }}" method="POST">
-                        {{ csrf_field() }}
-                        {{ method_field('DELETE') }}
+@section('content')
 
-                        <button>Delete Supplier</button>
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    @else
-    <div>No resutls</div>
-        @endif
-</div>
-</body>
-</html>
+    <!-- Bootstrap Boilerplate... -->
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    New Supplier
+                </div>
+            <div class="panel-body">
+                <form action="/suppliers" method="post" class="form-horizontal">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="supplier-name" class="col-sm-3 control-label"> Supplier name</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="name" id="supplier-name" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="supplier-web" class="col-sm-3 control-label"> Supplier Web</label>
+                        <div class="col-sm-6">
+                            <input type="text" name="web" id="supplier-web" class="form-control">
+                        </div>
+                    </div>
+                    <div>
+                        <div class="col-sm-offset-3 col-sm-6">
+                            <button type="submit" class="btn btn-default">
+                                <i class="fa fa-plus"></i>Add supplier
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+                <div>
+                    @if(count($suppliers1)>0)
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Current Supplier List
+                            </div>
+                        <div class="panel-body">
+                            <table class="table table-striped supplier-table">
+                                <thead>
+                                <th>Supplier</th>
+                                </thead>
+                                <tbody>
+                        @foreach($suppliers1 as $supplier)
+                            <tr>
+                                <!-- Supplier Name -->
+                                <td class="table-text">
+                                    <div>Supplier name : {{ $supplier->name }}</div>
+                                </td>
+                                <td class="table-text">
+                                   Supplier Web:  {{ $supplier->web }}
+                                </td>
+                                <!-- Supplier Delete Button-->
+                                <td>
+                                    <form action="/suppliers/{{ $supplier->id }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+
+                                        <button type="submit" class="btn btn-danger">
+                                            <i class="fa fa-btn fa-trash"></i>
+                                            Delete Supplier</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                                </tbody>
+                    @else
+                    <div>No resutls</div>
+                    @endif
+                    </table>
+                        </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
